943e25e71b2678949dd20a2430393e39b5750e9b
ChrisDelaPena <qcadelapena@tip.edu.ph>
2023-11-10 18:02:25 +0800

    simple ui change for login and register

--- main/screens/RegisterPage.js
+++ main/screens/RegisterPage.js
@@ -1,9 +1,10 @@
 import { useNavigation } from '@react-navigation/core'
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View, Image } from 'react-native'
 import React, { useState } from 'react'
 import AppStyles from '../constants/AppStyles'
 import { auth } from '../src/firebase'
 import { createUserWithEmailAndPassword } from 'firebase/auth';
+import logo from 'C:/Users/tipqc/au10_app/austin-app/main/assets/Au10GIF.gif'
 
 const RegisterPage = () => {
   const [email, setEmail] = useState('')
@@ -50,9 +51,10 @@ const RegisterPage = () => {
 
   return (
     <View style={styles.container}>
+      <Image source={logo} style={styles.mainlogo} />
       <View style={styles.logoContainer}>
-        <Text>
-          A.U.S.T.I.N.
+        <Text style={styles.textstyle}>
+          Advanced Utilization System for Tracking Insights and Numbers
         </Text>
       </View>
       <View style={styles.inputContainer}>
@@ -126,7 +128,7 @@ const styles = StyleSheet.create({
     padding: 30,
   },
   inputContainer: {
-    width: '80%',
+    width: 'flex',
   },
   input: {
     backgroundColor: AppStyles.color.primary,
@@ -149,7 +151,7 @@ const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: AppStyles.color.accent,
-    width: '100%',
+    width: 'flex',
     padding: 15,
     borderRadius: 10,
     alignItems: 'center',
@@ -177,4 +179,15 @@ const styles = StyleSheet.create({
     borderColor: 'red',
     borderWidth: 1,
   },
+  mainlogo: {
+    height: 130,
+    width: 130,
+    borderRadius: 100,
+    alignItems: 'baseline',
+  },
+  textstyle: {
+    textAlign: 'center',
+    fontSize: 17,
+    fontFamily: 'Iowan Old Style'
+  }
 });
